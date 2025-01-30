7e44d61768c4e5f04e0559507b0b72a1c81f41ad
Mohit Kumar Singh <mohit.kumar@nickelfox.com>
2025-01-31 00:35:05 +0530

    first commit

--- SwipeAssessment/SwipeAssessmentApp.swift
+++ SwipeAssessment/SwipeAssessmentApp.swift
@@ -13,7 +13,7 @@ struct SwipeAssessmentApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ProductListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
